211057ce01ba0d49141c64e541ee4c8314702c71
jk2429 <149831717+jk2429@users.noreply.github.com>
2024-03-21 16:11:35 -0700

    Updated/Add new files

--- C++/Controller.cpp
+++ C++/Controller.cpp
@@ -1,1 +1,60 @@
+//Import Libraries
+#include "HTMLChange.h"
+#include "UploadToGitHub.h"
+#include <iostream>
+#include <fstream>
+#include <string>
 
+using namespace std;
+
+bool testingHTMLChange()
+{
+	//Initalize Variables
+	fstream HTMLFile = fstream();
+	int priorityList[6] = { 5,3,1,0,4,2 };
+
+	HTMLFile.open("../../../HTML Files/scp_main_page.html");
+
+	bool temp = changeNavBar(&HTMLFile, priorityList);
+
+	HTMLFile.close();
+
+	return temp;
+}
+
+bool testingGitHub()
+{
+	//Initalize Variables
+	char localRepo[100] = "F:\Desktop\CS 386 Project\deployed_website\.git";
+	bool success = false;
+
+	cout << "launching repo.\n";
+
+	//Connect to local repo
+	if (!connectToRepository(localRepo))
+	{
+		return false;
+	}
+	
+	cout << "Pushing files to repo.\n";
+
+	//Push files
+	if (!pushFiles())
+	{
+		return false;
+	}
+
+	cout << "Freeing data\n";
+
+	//Free data
+	closeRepository();
+
+	return true;
+}
+
+int main()
+{
+	//Initialize Variables
+
+	return testingGitHub();
+}
---
+++ C++/UploadToGitHub.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef UPLOADTOGITHUB_H
+#define UPLOADTOGITHUB_H
+
+#include <git2.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+//Global Variables
+static git_repository* repo;
+static int error;
+
+//Forward Declare Functions
+bool connectToRepository(char* localRepo);
+bool pushFiles();
+void closeRepository();
+
+#endif
